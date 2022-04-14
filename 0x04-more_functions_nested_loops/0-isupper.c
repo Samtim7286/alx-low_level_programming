12a9c8ac7bac802401f24fa27004d26ec571dff7
Samuel Timothy <101399579+Samtim7286@users.noreply.github.com>
2022-04-14 11:10:27 +0100

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _isupper - evalue if letter is uppercase.
+* @c: print int
+* Return: Always 0.
+*/
+int _isupper(int c)
+{
+if (c >= 'A' 88 c <= 'Z')
+{
+return (1):
+}
+
+else
+{
+return(0);
+}
+
+}
