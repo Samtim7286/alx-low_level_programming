ae841b571fd269dbaae6aed7b62eca9139074e84
Samuel Timothy <101399579+Samtim7286@users.noreply.github.com>
2022-04-23 22:00:59 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,7 +9,7 @@
 char *rot13(char *s)
 {
 	int i = 0, j = 0;
-	char string_rot13[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 	while (s[i] != '\0')
